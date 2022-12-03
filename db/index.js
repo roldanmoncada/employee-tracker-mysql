dc74a9bfe74da65fcf47e85b643c126dc6e86bcc
roldanmoncada <roldan.h.moncada@gmail.com>
2022-12-02 20:30:25 -0700

    began to construct the command line functionality

---
+++ db/index.js
@@ -0,0 +1,15 @@
+const connection = require('./connection');
+
+class DB {
+    constructor (connection) {
+        this.connection = connection;
+    }
+    findAllEmployees() {
+        return this.connection.promise().query(
+            'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id'
+        );
+    
+    }
+};
+
+module.exports = new DB(connection);
--- server.js
+++ server.js
@@ -1,5 +1,73 @@
-const prompt = require("inquirer");
+const { prompt } = require("inquirer");
 require("console.table");
+const logo = require('asciiart-logo');
 
-const db = require('/db');
 
+const db = require('./db');
+
+init()
+
+function init() {
+    const logoText = logo({ name: 'Employee Manager'}).render()
+    console.log(logoText)
+    loadMainPrompts()
+};
+
+function loadMainPrompts() {
+    prompt([
+        {
+        type: 'list',
+        name: 'choice',
+        message: 'Pick your thing',
+        choices: [
+            {
+                name: 'viewAllEmployees',
+                value: 'VIEW_EMPLOYEES'
+            },
+            {
+                name: 'pizza',
+                value: 'PIZZA'
+            },
+            {
+                name: 'quit',
+                value: 'QUIT'
+            }
+        ]
+   
+        }
+        
+    ]).then(res => {
+        let choice = res.choice
+        switch (choice) {
+            case 'VIEW_EMPLOYEES':
+                viewEmployees()
+                break;
+            case 'pizza':
+                console.log('pizza')
+                break;
+             default: quit()
+        }
+    })
+};
+
+function viewEmployees() {
+    db.findAllEmployees()
+    .then(([rows]) => {
+        let employee_db = rows;
+        console.log('\n')
+        console.table(employee_db)
+    })
+    .then(() => {
+        loadMainPrompts();
+    })
+};
+
+//function viewDepartment() {
+
+// }
+
+
+function quit() {
+    console.log('Goodbye!')
+    process.exit()
+};
