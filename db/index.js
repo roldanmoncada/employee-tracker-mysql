97490152b0f65ac65ef7e0a0e89f65b9b250f154
roldanmoncada <roldan.h.moncada@gmail.com>
2023-01-18 16:49:32 -0700

    finished the query statements in the DB class on index.js.

--- db/index.js
+++ db/index.js
@@ -31,6 +31,67 @@ class DB {
             employeeID
         )
     }
+
+    findAllRoles() {
+        return this.connection.promise().query(
+            'SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON role.department_id = department.id;'
+        )
+    }
+
+    createEmployee(employee) {
+        return this.connection.promise().query(
+            'INSERT INTO employee SET ?',
+            employee
+        )
+    }
+
+    createRole(role) {
+        return this.connection.promise().query(
+            'INSERT INTO role SET ?',
+            role
+        )
+    }
+
+    createDepartment(department) {
+        return this.connection.promise().query(
+            'INSERT INTO department SET ?',
+            department
+        )
+    }
+
+    updateEmployeeRole(employeeID, roleID) {
+        return this.connection.promise().query(
+            'UPDATE employee SET role_id = ? WHERE id = ?',
+            [employeeID, roleID]
+        )
+    }
+
+    updateEmployeeManager(employeeID, managerID) {
+        return this.connection.promise().query(
+            'UPDATE employee SET manager_id = ? WHERE id = ?',
+            [employeeID, managerID]
+        )
+    }
+
+    viewDepartmentBudgets() {
+        return this.connection.promise().query(
+            'SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id GROUP BY department.id, department.name;'
+        )
+    }
+
+    removeEmployee(employeeID) {
+        return this.connection.promise().query(
+            'DELETE from employee WHERE id = ?',
+            employeeID
+        )
+    }
+
+      removeDepartment(departmentID) {
+        return this.connection.promise().query(
+            'DELETE from department WHERE id = ?',
+            departmentID
+        )
+    }
 };
 
 module.exports = new DB(connection);
